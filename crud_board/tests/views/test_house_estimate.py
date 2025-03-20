e3d83f6c3f8d844e919e4fe58355a75d2260f384
s.sagara <>
2025-03-20 19:42:07 +0900

    ファイル名の修正

--- crud_board/tests/views/test_house_estimate.py
+++ crud_board/tests/views/test_house_estimate.py
@@ -1,16 +1,17 @@
-from django.test import TestCase, Client
+from django.test import Client, TestCase
 from django.urls import reverse
-from crud_board.models import HouseEstimate
+
 from crud_board.forms import HouseEstimateForm
+from crud_board.models import HouseEstimate
 
 
-class HouseEstimateViewTests(TestCase):
+class TestsHouseEstimateView(TestCase):
 
     def setUp(self):
         self.client = Client()
 
     def test_new_get(self):
-        response = self.client.get(reverse('crud_board:house_estimate_new'))
+        response = self.client.get(reverse("crud_board:house_estimate_new"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'house_estimate/new.html')
-        self.assertIsInstance(response.context['form'], HouseEstimateForm)
+        self.assertTemplateUsed(response, "house_estimate/new.html")
+        self.assertIsInstance(response.context["form"], HouseEstimateForm)
